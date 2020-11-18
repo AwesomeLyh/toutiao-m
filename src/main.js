630815ec3b5756b2b1ada2de6d52ae1aab77917d
AwesomeLi <AwesomeLiwork@163.com>
2020-11-18 10:21:57 +0800

    初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import axios from "axios";
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
 
-Vue.prototype.$http = axios;
+import './styles/index.css'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
