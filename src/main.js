d1d2930f082e2485c6429d2c54f3555a8008d4e6
AwesomeLi <AwesomeLiwork@163.com>
2020-11-18 11:29:06 +0800

    引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,33 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
 
-import './styles/index.css'
+import 'nprogress/nprogress.css'
+// 全局样式
+import './styles/index.less'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/app/'
+
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config.data
+})
 
 Vue.prototype.$http = axios
 
